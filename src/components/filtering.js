1d45304db904b53dea69291c8826e0df576039b2
Elina Shogenova <elina.shog04@gmail.com>
2026-01-23 09:58:57 +0300

    Проектнач работа 6

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,33 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                      // используйте name как значение и текстовое содержимое
+                        const option = document.createElement('option');    // @todo: создать и вернуть тег опции
+                        option.value = name;
+                        option.textContent = name;
+                        return option;
+                      })
+        )
+     })
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const fieldName = action.dataset.field;
+            const parent = action.closest('label');
+            const input = parent.querySelector(`input[name="${fieldName}"]`);
+            input.value = '';
+            state[fieldName] = '';
+        }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,15 @@
-import {rules, createComparison} from "../lib/compare.js";
-
+import {rules, defaultRules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const ruleNames = [defaultRules[1]];
+    const customRules = [rules.searchMultipleFields (searchField, ['date', 'customer', 'seller'], false)];
+    const comparator = createComparison(ruleNames, customRules);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(item => comparator(item, state));
     }
 }
+
+
